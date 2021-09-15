07798b5d706ab539eab42dee65b911544f0b7f6e
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-09-15 10:58:24 +0100

    add all pages and link

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-const show = document.querySelector()
-function show() {
-  document.querySelector('.sidebar').classList.toggle('show');
-}
-
-const burger = document.querySelector('.fas');
+window.onload = () => {
+  const burger = document.querySelector('.burger');
 
   burger.addEventListener('click', (event) => {
     event.preventDefault();
-    const mobileMenu = document.querySelector('.sidebar');
-    mobileMenu.classList.toggle('show');
-  });
-  const mobileMenuList = document.querySelector('.sidebar');
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.add('showww');
 
-  mobileMenuList.addEventListener('click', () => {
-    const mobileMenu = document.querySelector('.body');
-    mobileMenu.classList.toggle('show');
+    console.log (sidebar);
   });
 
+  const iconx = document.querySelector('.closeb');
+  console.log (iconx);
 
-//function show() {
- // document.querySelector('.sidebar').classList.toggle('.active');}
+  iconx.addEventListener('click', () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('showww');
+    
+    
+  });
+
+  const body = document.querySelector('.introduction');
+
+  console.log(body);
+
+  body.addEventListener("click", () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classlist.remove("showww");
+  });
+  
+
+  const sidebarlist = document.querySelector('.sidebar ul li');
+
+  sidebarlist.addEventListener('click', () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('showww');
+  });
+  console.log(sidebarlist);
+};
---
+++ speakers.js
@@ -0,0 +1,33 @@
+const speakers = [
+  {
+    name: 'Ringobel song',
+    image: '',
+    text: 'Magni consequuntur, vel ipsa possimus corporis accusamus magnam facere. Facere enim incidunt',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis !',
+  },
+  {
+    name: 'Boma patrick',
+    image: '',
+    text: 'Magni consequuntur, vel ipsa possimus corporis accusamus magnam facere. Facere enim incidunt',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis !',
+  },
+  {
+    name: 'Amhad',
+    image: '',
+    text: 'Magni consequuntur, vel ipsa possimus corporis accusamus magnam facere. Facere enim incidunt',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis !',
+  },
+  {
+    name: 'patrice motsepe',
+    image: '',
+    text: 'Magni consequuntur, vel ipsa possimus corporis accusamus magnam facere. Facere enim incidunt',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis !',
+  },
+  {
+    name: 'Paul Biya',
+    image: './i',
+    text: 'Magni consequuntur, vel ipsa possimus corporis accusamus magnam facere. Facere enim incidunt',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis perspiciatis !',
+  },
+  
+];
